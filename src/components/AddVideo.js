949588ba0e881f2da260b4bd281efed35f929ada
MOHD SAMIULLAH <samiullah0813@gmail.com>
2024-07-05 01:12:16 +0530

    ui improved

--- src/components/AddVideo.js
+++ src/components/AddVideo.js
@@ -1,8 +1,8 @@
 // src/components/AddVideo.js
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addVideo } from '../features/videoSlice';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addVideo } from "../features/videoSlice";
+import { v4 as uuidv4 } from "uuid";
 
 function AddVideo() {
   const dispatch = useDispatch();
@@ -26,19 +26,21 @@ function AddVideo() {
   };
 
   return (
-    <div className="flex flex-col items-center p-4 bg-gray-100 rounded-md shadow-md">
-      <input
-        type="file"
-        accept="video/*"
-        onChange={handleVideoChange}
-        className="mb-4"
-      />
-      <button
-        onClick={handleAddVideo}
-        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-      >
-        Add Video
-      </button>
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center p-4 w-1/2 bg-white mt-12 rounded-md shadow-md">
+        <input
+          type="file"
+          accept="video/*"
+          onChange={handleVideoChange}
+          className="mb-4"
+        />
+        <button
+          onClick={handleAddVideo}
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+        >
+          Add Video
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/BookmarkFilter.js
+++ src/components/BookmarkFilter.js
@@ -16,10 +16,12 @@ function BookmarkFilter() {
     <div className="flex flex-col items-center p-4">
       <button
         onClick={handleToggleFilter}
-        className="bg-purple-500 text-white py-2 px-4 rounded mb-4 hover:bg-purple-600"
+        className="bg-purple-500 text-white py-2 px-4 mt-4 rounded mb-4 hover:bg-purple-600"
       >
         {showBookmarks ? 'Show All Videos' : 'Show Bookmarked Videos'}
       </button>
+      <p class="text-center my-4 font-medium text-slate-500">{showBookmarks ? 'Bookmarked videos' : 'List of all videos'}</p>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {(showBookmarks ? bookmarks : videos).map((video) => (
           <VideoItem key={video.id} video={video} />
