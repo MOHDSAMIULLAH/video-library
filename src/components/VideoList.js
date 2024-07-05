b5f9ba2d08cc623412e335bef0daee80895a9f9d
MOHD SAMIULLAH <samiullah0813@gmail.com>
2024-07-05 18:54:59 +0530

    unnecessary code removed

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import VideoItem from './VideoItem';
+import React from "react";
+import { useSelector } from "react-redux";
+import VideoItem from "./VideoItem";
 
 function VideoList() {
   const videos = useSelector((state) => state.videos.videos);
 
   return (
     <div className="flex flex-col items-center p-4">
-        
-        {/* <h2 className="bg-pink-500 text-white py-2 px-4 rounded mb-4 ">List of All Videos </h2> */}
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {videos && videos.map((video) => (
-          <VideoItem key={video.id} video={video} />
-        ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {videos &&
+          videos.map((video) => <VideoItem key={video.id} video={video} />)}
+      </div>
     </div>
-        </div>
   );
 }
 
